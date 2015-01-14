aa0f330e6bbc381a1657f3493f7ca459490155a7
Sibert Aerts <sibert.aerts@gmail.com>
2015-01-14 17:59:36 +0100

    Movies 3.1
    The Paranormal Phantom & Global Odyssey 2: Requiem for a Fallen Lie

--- Test/movies.py
+++ Test/movies.py
@@ -1,24 +1,26 @@
 import randomrng = random.Random()
 # Collection of nouns that describe concepts
 movieTitleConcepts = [ "Life", "Death", "Power", "Strength", "Wisdom", "Defiance", "Truth",
-							"Lies", "Acceptance", "Forgiveness", "Murder", 
+							"Acceptance", "Forgiveness", "Murder", "Terror", "Liberty",
 							"Joy", "Sorrow", "Fury", "Fear", "Pain", "War", "Honor", "Duty", "Guardian"]
 
 # Collection of nouns that describe objects
-movieTitleObjects = 	[ "Dawn", "Burn", "Story", "Legacy", "Birth", "Galaxy", "Guardian",
+movieTitleObjects = 	[ "Dawn", "Burn", "Story", "Legacy", "Birth", "Galaxy", "Guardian", "Hero",
 							"Interview",  "Grit", "Cop", "Shark", "Room", "Fight", "Knife", "America",
 							"Shank", "Thing", "Snake", "Gun", "Gear", "Tendency", "Sword", "Russia",
-							"Blade", "Godfather", "Odyssey", "Walker", "Boyhood", "Activity",
-							"Massacre", "Chainsaw", "Eater", "Fist", "Werewolf", "Vampire", "Boss"]
+							"Blade", "Godfather", "Odyssey", "Walker", "Boyhood", "Activity", "Lie",
+							"Massacre", "Chainsaw", "Eater", "Fist", "Werewolf", "Vampire", "Boss",
+							"Crusader"]
 # Collection of nounsmovieTitleNouns = movieTitleConcepts + movieTitleObjects
 # Collection of adjectivesmovieTitleAdnouns = [ "Hot", "Cold", "Light", "Gone", "Big",  "Dark", "Pacific", "Bold", 							"Horrible", "True", "Solid", "Golden", "Red", "Scary", "Paranormal",
 							"Black", "Unbreakable", "American", "Russian", "Righteous", "Heroic",
-							"Dangerous", "Lethal", "Global", "Viral", "Real"]# Collection of nouns that double as adjectives, and adjectives that double as nounsmovieTitleDoubles = [ "Alien", "Liberty", "Hobbit", "Monster", "Robot", "American", "Russian",							"Cyborg", "Ninja", "Machine", "Predator", "Stone", "Steel", "Space", 							"Diamond", "Metal", "Fear", "Death", "Blood", "Heat", "Fallen", 							"Peace", "Ghost", "Phantom", "Battle", "Enemy", "Hostile", "Expendable",
-							"Mutant", "Nightmare", "Texas"]
+							"Dangerous", "Lethal", "Global", "Viral", "Real", "Bloody", "Gory", "Deadly",
+							"Deep-Space", "Violent", "Large"]# Collection of nouns that double as adjectives, and adjectives that double as nounsmovieTitleDoubles = [ "Alien", "Liberty", "Hobbit", "Monster", "Robot", "American", "Russian",							"Cyborg", "Ninja", "Machine", "Predator", "Stone", "Steel", "Space", 							"Diamond", "Metal", "Fear", "Death", "Blood", "Heat", "Fallen", 							"Peace", "Ghost", "Phantom", "Battle", "Enemy", "Hostile", "Expendable",
+							"Mutant", "Nightmare", "Texas", "Stardust"]
 							
 
 							
-# Collection of starts to a movie subtitlemovieSubtitleStarts = ["Revenge of", "Attack of", "Return of",							"Story of", "A Tale of", "Requiem for", "A New",							"End of the", "Eternal Sunshine of", "Call for", "Call of",							"Death of the", "Passion of", "Memories of", "The Phantom",							"Sons of the", "Guns of the", "Prepare for", "The Missing", "Lost",							"A Dystopian", "Unlimited", "Grandpa's", "A Space", "The Dark",							"The Smell of", "The Final", "Enter the", "The Usual", "The Big"]
+# Collection of starts to a movie subtitlemovieSubtitleStarts = ["Revenge of the", "Attack of the", "Return of the",							"Story of the", "A Tale of the", "Requiem for a", "A New",							"End of the", "Eternal Sunshine of the", "Call for", "Call of",							"Death of the", "Passion of the", "Memories of", "The Phantom",							"Sons of the", "Guns of the", "Prepare for", "The Missing", "The Lost",							"A Dystopian", "Unlimited", "Grandpa's", "A Space", "The Dark",							"The Smell of", "The Final", "Enter the", "The Usual", "The Big"]
 
 # Collection of ends to a movie subtitlemovieSubtitleEnds = ["Strikes Back", "Harder", "Menace", "Awakens", 							"Annihilation", "of Power", "of Fury", "With a Cause",							"Again", "Better", "Faster", "Stronger", "Justice",							"Wizard", "Redemption", "Vengeance", "of Liberty",							"from Space", "from Beneath", "from Hell", "After Dark"]						def chance_out_of(x, y):
 	""" Has X chance out of Y to return True. """	return rng.randint(1, y) <= x						def flip_coin():
@@ -43,35 +45,36 @@ def make_multiple(string):
 	else:
 		string += "s"
 	return string
-def generate_title_element():
+def generate_title_element(phrase = True):
 	""" Generates a title element consisting of a noun and random additional elements """
 	result = ""
-	if chance_out_of(3,4):	# adjective? noun
+	if chance_out_of(3,4):					# adjective? noun
 		if flip_coin():
 			result += random_elem(movieTitleAdnouns + movieTitleDoubles)
 			result += " "
 		result += random_elem(movieTitleNouns + movieTitleDoubles)
-		if chance_out_of(1,4): 		# adjective? nouns
+		if chance_out_of(1,4): 					# adjective? nouns
 			result = make_multiple(result)
-	elif flip_coin():			# noun is adjective
+	elif flip_coin() and phrase:			# noun is adjective
 		result += random_elem(movieTitleNouns + movieTitleDoubles)
 		if chance_out_of(3,4):
 			result += " is "
-		else: 						# nouns are adjective
+		else: 										# nouns are adjective
 			result = make_multiple(result)
 			result += " are "
 		result += random_elem(movieTitleAdnouns)
-	elif flip_coin():			# concept of nouns
+	elif flip_coin():							# concept of nouns
 		result += random_elem(movieTitleConcepts) + " of " + random_elem(movieTitleNouns + movieTitleDoubles)
 		result = make_multiple(result)
-	else:							# noun of concept
+	else:											# noun of concept
 		result += random_elem(movieTitleObjects + movieTitleDoubles) + " of " + random_elem(movieTitleConcepts)
-		if chance_out_of(1,4): 		# noun of concepts
+		if chance_out_of(1,4): 					# noun of concepts
 			result = make_multiple(result)
 	return result
 			def generate_movie_title():	""" Generates a (somewhat plausible) movie title. """	result = ""
-	if flip_coin(): result += random_elem(["The", "The", "The", "Beyond", "Way of the", "Life of the",]) + " "
-		if chance_out_of(1, 4):	# Title & title
-		result += generate_title_element()		result += " " + random_elem(["after", "beyond", "through", "with", "Vs", "and", "&", "despite"]) + " "
-			result += generate_title_element()		if flip_coin() or len(result) < 20:	# number? : Subtitle		if flip_coin(): result += " " + str(rng.randint(2, 6)) 		result += ": "		if flip_coin(): result += random_elem(movieSubtitleStarts) + " "
-		result += generate_title_element()		if (flip_coin() and len(result) < 50) or len(result) < 30 : result += " " + random_elem(movieSubtitleEnds) 			return resultfor x in range(0, 10):	print( generate_movie_title())
+	if flip_coin(): result += random_elem(["The", "The", "The", "Beyond", "Way of the", "Life of the",]) + " "	if chance_out_of(1, 4):	# Title & title
+		result += generate_title_element()		result += " " + random_elem(["after", "beyond the", "through", "with", "Vs", "and the", "&", "despite the"]) + " "
+			result += generate_title_element( len(result) < 8 )		if flip_coin() or len(result) < 20:	# number? : Subtitle		if flip_coin(): result += " " + str(rng.randint(2, 6)) 		result += ": "		if flip_coin(): result += random_elem(movieSubtitleStarts) + " "
+	
+		result += generate_title_element(False)
+			if (flip_coin() and len(result) < 50) or len(result) < 30 : result += " " + random_elem(movieSubtitleEnds) 			return resultfor x in range(0, 10):	print( generate_movie_title())
